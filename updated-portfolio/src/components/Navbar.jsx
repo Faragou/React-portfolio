85e810fdefbd0787702f0610f6ff38b2f0c24ac5
farag <farago.patrik2002@gmail.com>
2024-11-10 16:43:38 +0100

    Finishing up responsivity

--- updated-portfolio/src/components/Navbar.jsx
+++ updated-portfolio/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React, { forwardRef, useEffect, useRef } from 'react';
+import React, { forwardRef, useEffect, useRef, useState } from 'react';
 import { useSticky } from './StickyContext';
 import './Navbar.scss';
 
 const Navbar = forwardRef(() => {
+  const [isOpen, setIsOpen] = useState(false); // State for mobile menu
   const navbarRef = useRef(null);
    const spacerRef = useRef(null);
   const { isSticky } = useSticky();
@@ -26,11 +27,14 @@ const Navbar = forwardRef(() => {
       }
     };
   }, [setIsSticky]);
-
+ const toggleMenu = () => setIsOpen(!isOpen);
   return (
     <>
     <div ref={spacerRef} className="navbar-spacer"></div> 
-    <nav ref={navbarRef} id="topNav" className={`navbar ${isSticky ? 'sticky' : ''}`}>
+    <nav ref={navbarRef} id="topNav" className={`navbar ${isSticky ? 'sticky' : ''} ${isOpen ? 'open' : ''}`}>
+     <div className="menu-toggle" onClick={toggleMenu}>
+          ☰ {/* Hamburger icon */}
+      </div>
       <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
